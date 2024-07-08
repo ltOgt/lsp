ef8ef241210ec9f71ed43c3d620ddb860017d504
ltOgt <omnesiaorg@gmail.com>
2024-07-08 21:33:03 +0200

    expose dart execution path

--- lib/src/connector/dart_connector.dart
+++ lib/src/connector/dart_connector.dart
@@ -6,10 +6,14 @@ class LspConnectorDart extends LspConnectorBase {
   /// The path to the dart analysis server executable.
   final String analysisServerPath;
 
+  // The path to the dart executable to run the analysis server from
+  final String dartExecPath;
+
   LspConnectorDart({
     required this.analysisServerPath,
     required String clientId,
     required String clientVersion,
+    this.dartExecPath = "dart", // if its in the $PATH
   }) : super(clientId: clientId, clientVersion: clientVersion);
 
   @override
@@ -17,7 +21,7 @@ class LspConnectorDart extends LspConnectorBase {
     // https://stackoverflow.com/a/67797686/7215915 for how to use this on apple
     // Also see https://github.com/flutter/flutter/issues/89837#issuecomment-2099544981
     return Process.start(
-      "dart",
+      dartExecPath,
       [analysisServerPath, "--lsp", "--client-id", clientId, "--client-version", clientVersion],
     );
   }
